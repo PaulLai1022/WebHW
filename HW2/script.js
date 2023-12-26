92f90c2a6fec238afad553eee47fc46ca3e436b2
PaulLai1022 <71100835+PaulLai1022@users.noreply.github.com>
2023-12-26 17:32:32 +0900

    HW2

---
+++ HW2/script.js
@@ -0,0 +1,10 @@
+var button = document.getElementsByTagName("button")[0];
+var area = document.getElementsByTagName('div');
+
+
+
+function printFunction(){
+    
+    let date = Date();
+    document.getElementById('area').innerHTML=date;
+}
